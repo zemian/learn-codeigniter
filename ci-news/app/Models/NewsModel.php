10fed801cd50aeda649f2a92f3c79b83756ab113
Zemian Deng <zemiandeng@gmail.com>
2020-10-21 08:43:49 -0400

    Add create news

--- ci-news/app/Models/NewsModel.php
+++ ci-news/app/Models/NewsModel.php
@@ -6,6 +6,8 @@ class NewsModel extends Model
 {
     protected $table = 'news';
 
+    protected $allowedFields = ['title', 'slug', 'body'];
+
     public function getNews($slug = false)
     {
         if ($slug === false)
